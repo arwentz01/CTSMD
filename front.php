59e4d98d2734bf11252bfa53e91cf8f02846941a
arwentz01 <arwentz01@gmail.com>
2026-08-07 09:44:51 -0400

    Route Teams and private channels

--- front.php
+++ front.php
@@ -83,6 +83,11 @@ if (VolunteerExperience::handles($route)) {
     VolunteerExperience::render($route, $detectedBasePath);
 }
 
+require_once __DIR__ . '/src/TeamExperience.php';
+if (TeamExperience::handles($route)) {
+    TeamExperience::render($route, $detectedBasePath);
+}
+
 require_once __DIR__ . '/src/CommunityManagementExperience.php';
 if (CommunityManagementExperience::handles($route)) {
     CommunityManagementExperience::render($route, $detectedBasePath);
